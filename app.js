5c190e979956a4f4386a207af570d48651e6352c
thisisal1ev <aaalievvv1@gmail.com>
2023-11-03 15:11:39 +0500

    Iniatial commit

---
+++ app.js
@@ -0,0 +1,64 @@
+const elTitle = document.querySelector('h1')
+const elChangeTextBtn = document.querySelector('button')
+elChangeTextBtn.addEventListener('click', function () {
+    elTitle.textContent = 'JavaScript';
+    elChangeTextBtn.textContent = 'Button'
+})
+
+let son1 = 15;
+let son2 = 5;
+
+console.log(son1 <= son2);
+
+let ism = "Axmadillo";
+let words = `Salom ${ism}!`
+console.log(words);
+
+let soz1 = true
+let soz2 = false
+console.log(!soz1);
+
+const html = true;
+const css = true;
+let sms = '';
+
+if (html && css) {
+    sms = 'Bootstrapga otishga ruhsat'
+} else if (html || css) {
+    sms = 'Cssni tugatvo krisa'
+} else {
+    sms = 'html va css tugatvo birinchi!'
+};
+
+console.log(sms);
+
+const functionPlus = function (son1, son2) {
+    return son1 + son2
+};
+
+console.log(functionPlus(10, 25));
+
+const kvadrat = function (son) {
+    return son * son
+};
+
+console.log(kvadrat(5));
+
+const mevalar = ['gilos', 'olma', 'orik'];
+const boshmi = mevalar.length === 0;
+const elText = document.querySelector('.elText2');
+if (boshmi) {
+    elText.textContent = 'Savatcha bosh'
+} else {
+    elText.textContent = `Savtcha ${mevalar.length}ta mahsulot`
+}
+
+const sonlar = prompt('ragam kiriting')
+if (sonlar % 2 === 0) {
+    console.log(`${sonlar} - juft son`);
+} else if (sonlar % 2 === 1) {
+    console.log(`${sonlar} - toq son`);
+}
+else {
+    console.log(`Bor ashag'da oyna`);
+}
